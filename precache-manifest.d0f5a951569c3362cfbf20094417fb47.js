4e3d4cd34d3452446f43ee06c82badaec2b0ffb1
mohamed gamal <mohamedgamaal20144@gmail.com>
2019-11-30 19:57:22 +0200

    First React App

---
+++ precache-manifest.d0f5a951569c3362cfbf20094417fb47.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c727f5611c8cc9ceec4638881976dd5b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d114396367d1befbf41f",
+    "url": "/static/css/main.1c0afb20.chunk.css"
+  },
+  {
+    "revision": "caa37e2a918499e99005",
+    "url": "/static/js/2.42310ece.chunk.js"
+  },
+  {
+    "revision": "d114396367d1befbf41f",
+    "url": "/static/js/main.c67e6e68.chunk.js"
+  },
+  {
+    "revision": "3c82f2d01e28496ba67e",
+    "url": "/static/js/runtime~main.5132f812.js"
+  },
+  {
+    "revision": "08494abf4210d1ea8b163791db70773c",
+    "url": "/static/media/reactMovie_logo.08494abf.png"
+  }
+]);
